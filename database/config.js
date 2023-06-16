066d31cb9fdf10e44e1549839b5c585ee872267e
NiicoVarelaa <niicovarelaa@gmail.com>
2023-06-15 22:38:17 -0300

    configuraciones adicionales bd mongo

--- database/config.js
+++ database/config.js
@@ -2,10 +2,15 @@ const mongoose = require('mongoose');
 
 require('dotenv').config();
 
+const credenciales = process.env.DB_CONECCTION;
+console.log('db',credenciales);
 
 const dbConecction = async () => {
-    try {
-        await mongoose.connect(process.env.DB_CONECCTION);
+    try {   
+        await mongoose.connect(process.env.DB_CONECCTION, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true
+        });
         console.log("DB Conectado");
     } catch (error) {
         console.log(error);
